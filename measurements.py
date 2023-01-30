438e171b191fec9c4d412b4a3fe895f0e11792ae
roddydr123 <davidroddy321@gmail.com>
2023-01-30 17:21:53 +0000

    add looper

---
+++ measurements.py
@@ -0,0 +1,56 @@
+from physics import Ising
+import numpy as np
+from tqdm import tqdm
+
+J=1.0
+nstep=10100
+wait=100
+
+lx=50
+ly=lx
+
+method = "G"
+
+def onerun(kT):
+    # no visualisation
+    # run a sim for 10000 sweeps
+    # start measurements after 100 sweeps
+    # record the magnetisation and susceptibility every 10 sweeps
+
+    model = Ising(lx, kT, method)
+
+    runfile = f"{kT}output.dat"
+
+    # clear file
+    f = open(runfile, "w")
+    f.close()
+
+    for n in tqdm(range(nstep)):
+        for i in range(lx):
+            for j in range(ly):
+                if method == "G":
+                    model.updateSpinsGlauber()
+                if method == "K":
+                    model.updateSpinsKawasaki()
+
+        if n % 10 == 0 and n > wait:
+
+            total_magnetisation = model.get_total_magnetisation()
+            total_energy = model.get_total_energy()
+            susceptibility = model.get_susceptibility()
+            
+            # record required data
+            with open(runfile, "a") as file:
+                file.write(f"{total_magnetisation}, {total_energy}, {susceptibility}\n")
+
+
+def main():
+
+    # make array of all the temperatures to loop through
+    temps = np.arange(1, 3, 0.1)
+
+    for temp in tqdm(temps):
+        onerun(temp)
+
+
+main()
