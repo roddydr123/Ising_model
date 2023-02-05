4bc3f537f15937a526ebf245cc4d18ee93603f45
roddydr123 <davidroddy321@gmail.com>
2023-02-05 23:13:54 +0000

    working visualisation

--- vis.py
+++ vis.py
@@ -1,24 +1,22 @@
 import sys
 import matplotlib.pyplot as plt
 from physics import Ising
-import numpy as np
 
 J=1.0
 nstep=500
 
 if(len(sys.argv) != 4):
-    print ("Usage python ising.animation.py N T method")
+    print ("Usage python vis.py N T method")
     sys.exit()
 
 lx=int(sys.argv[1]) 
-ly=lx 
+ly=lx
 kT=float(sys.argv[2])
 method = sys.argv[3]
 
-model = Ising(lx, kT, method)
-spins = model.spins
+model = Ising(lx, kT, method, nstep, spins="random")
 fig, ax = plt.subplots()
-im=ax.imshow(spins, animated=True)
+im=ax.imshow(model.spins, animated=True)
 
 for n in range(nstep):
     for i in range(lx):
@@ -27,21 +25,10 @@ for n in range(nstep):
                 model.updateSpinsGlauber()
             if method == "K":
                 model.updateSpinsKawasaki()
+            model.count += 1
 
-    if(n%10==0): 
+    if(n%10==0):
         fig.canvas.flush_events()
-        spins = model.spins
-        im=ax.imshow(spins, animated=True)
+        im=ax.imshow(model.spins, animated=True)
         ax.draw_artist(im)
         plt.pause(0.011)
-        model.get_total_magnetisation()
-        model.get_total_energy()
-
-av_sp_heat = model.get_heat_capacity()
-av_energy = np.average(model.energy_list)
-av_magnetisation = np.average(model.magnetisation_list)
-av_susceptibility = model.get_susceptibility()
-
-c_error = model.get_bootstrap_error(200, 50, "c")
-
-print(f"{kT}, {av_sp_heat}, {c_error}, {av_energy}, {av_magnetisation}, {av_susceptibility}")
