86cd7d075f70158db86e3cc9ec6662513356cbfb
roddydr123 <davidroddy321@gmail.com>
2023-02-05 19:03:24 +0000

    run sim

--- measurements.py
+++ measurements.py
@@ -3,7 +3,7 @@ import numpy as np
 from tqdm import tqdm
 
 J=1.0
-nstep=150
+nstep=10100
 wait=100
 
 lx=50
@@ -11,7 +11,7 @@ ly=lx
 
 method = "G"
 
-run_name = "ghgh"
+run_name = "G1"
 
 def onerun(kT, model):
 
