d03f813e9a833b57b57797ac604853b7aec8b51d
roddydr123 <davidroddy321@gmail.com>
2023-02-05 18:40:33 +0000

    changes

--- manip.py
+++ manip.py
@@ -1,26 +1,48 @@
 import numpy as np
+from physics import Ising
 
-temperature = 1
+
+n = 200
+k = 50
+
+temps = np.round(np.arange(1, 3, 0.1), 3)
+
+for temp in temps:
+
+    model = Ising(50, temp, "G", 10100)
+    data = np.genfromtxt(f"data/outfile{temp}.dat")
+
+    energies_list = data.T[1]
+    magnetisation_list = data.T[0]
+
+    quantity = []
+
+    for i in range(k):
+        mags = np.random.choice(magnetisation_list, n)
+        quantity.append(model.get_susceptibility(magnetisations=mags))
+
+    g = np.average(quantity)**2
+    h = np.average(np.array(quantity)**2)
+    error = np.sqrt(h-g)
+
+    print(error)
 
 
-data = np.genfromtxt("outfile2.4.dat")
 
-energies = data.T[1]
-mags = data.T[0]
 
-av_energy = np.average(energies)
-av_mag = np.average(mags)
 
-print(av_energy, av_mag)
 
-norm = 1/(50**2 * 1)
-sus = norm * (np.average(np.square(mags)) - np.square(np.average(mags)))
-C = norm * (np.average(np.square(energies)) - np.square(np.average(energies)))
+# av_energy = np.average(energies)
+# av_mag = np.average(mags)
 
-C = 1/(temperature * 2500) * (np.var(energies))
+# print(av_energy, av_mag)
 
-sus = 1/(temperature * 2500) * (np.var(mags))
+# norm = 1/(50**2 * 1)
+# sus = norm * (np.average(np.square(mags)) - np.square(np.average(mags)))
+# C = norm * (np.average(np.square(energies)) - np.square(np.average(energies)))
 
-print(sus, C)
+# C = 1/(temperature * 2500) * (np.var(energies))
 
-H_errs.append(np.std( np.square(np.array(E_vals) - e_avg) / (no_particles * T**2)) / len(E_vals))
+# sus = 1/(temperature * 2500) * (np.var(mags))
+
+# print(sus, C)
